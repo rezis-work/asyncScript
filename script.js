370111c43ad69e72f60240641e374f5f6d55f660
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-04-16 22:29:22 +0400

    make api call for countries using xml

--- script.js
+++ script.js
@@ -5,4 +5,74 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [_, data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.png}" alt="${
+      data.flags.alt
+    }" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} M</p>
+        <p class="country__row"><span>🗣️</span>${data.languages.kat}</p>
+        <p class="country__row"><span>💰</span>${data.currencies.GEL.name}</p>
+      </div>
+    </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('Georgia');
+
+const getCountryDataDemo = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.png}" alt="${
+      data.flags.alt
+    }" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} M</p>
+        <p class="country__row"><span>🗣️</span>${
+          Object.keys(data.languages)[0]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          Object.keys(data.currencies)[0]
+        }</p>
+      </div>
+    </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryDataDemo('usa');
+getCountryDataDemo('italy');
+getCountryDataDemo('germany');
